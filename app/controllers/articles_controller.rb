ded51b550635bd3c9f59a901e6331aadc0508efc
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-10-03 18:25:00 +0300

    commit tree 53a84322437c403c86368e8d473d1b8df2013374

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,8 @@ class ArticlesController < ApplicationController
     render json: articles, each_serializer: ArticleListSerializer
   end
 
+  
+
   def show
     article = Article.find(params[:id])
     render json: article
